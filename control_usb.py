ddf4a18c5892f080eab9ddc049c8241bd6071f90
smartexpt <antonio.rocha@smartex.pt>
2018-11-22 14:48:38 +0000

    save changes (3)

--- control_usb.py
+++ control_usb.py
@@ -13,7 +13,6 @@ def powerOnUSBs():
                         shell=True, stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
-    proc.communicate('{}\n'.format('lycralycra'))
     print('Booted on USB ports.')
 
 if __name__ == "__main__":
